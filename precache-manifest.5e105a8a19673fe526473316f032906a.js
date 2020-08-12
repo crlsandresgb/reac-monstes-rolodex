be53d2ce1b909a6d2d05208b6c94312c37fb1ae1
carlos <crls.andres.gb@gmail.com>
2020-08-11 19:12:39 -0600

    Updates

---
+++ precache-manifest.5e105a8a19673fe526473316f032906a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206a67d8868d143507c880de20fc4861",
+    "url": "/reac-monstes-rolodex/index.html"
+  },
+  {
+    "revision": "bd94b6dc1975b5fd52b7",
+    "url": "/reac-monstes-rolodex/static/css/main.42b984b8.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/reac-monstes-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reac-monstes-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd94b6dc1975b5fd52b7",
+    "url": "/reac-monstes-rolodex/static/js/main.e952865e.chunk.js"
+  },
+  {
+    "revision": "21a53e201f34a2c4d2cd",
+    "url": "/reac-monstes-rolodex/static/js/runtime-main.5d6ddc23.js"
+  }
+]);
